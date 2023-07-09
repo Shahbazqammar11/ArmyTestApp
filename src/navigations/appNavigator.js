fb55fd0bd1636414d3485db0e0c34e89a67a2a4f
shahbazqammar <shahbazqammar06@gmail.com>
2023-07-09 12:07:30 +0500

    saving

--- src/navigations/appNavigator.js
+++ src/navigations/appNavigator.js
@@ -2,10 +2,10 @@ import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "../screens/login";
-import Signup from "../screens/signup";
-import Home from "../screens/home";
-import ForgotPassword from "../screens/forgotpassword";
+import Login from "../screens/Login";
+import Signup from "../screens/Signup";
+import Home from "../screens/Home";
+import ForgotPassword from "../screens/Forgotpassword";
 
 const Stack = createNativeStackNavigator();
 
