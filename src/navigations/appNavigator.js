a18191400715fd41af9130434d08521b35ec6899
shahbazqammar <shahbazqammar06@gmail.com>
2023-07-09 12:06:42 +0500

    stack navigation instllation and setting in app navigator

--- src/navigations/appNavigator.js
+++ src/navigations/appNavigator.js
@@ -1,10 +1,24 @@
 import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import Login from "../screens/login";
+import Signup from "../screens/signup";
+import Home from "../screens/home";
+import ForgotPassword from "../screens/forgotpassword";
+
+const Stack = createNativeStackNavigator();
 
 function AppNavigator() {
   return (
-    <NavigationContainer>{/* Rest of your app code */}</NavigationContainer>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
