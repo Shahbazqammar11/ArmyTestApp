9af1d4fe16abd59d993ca3ceedf8514f0447975b
shahbazqammar <shahbazqammar06@gmail.com>
2023-09-01 14:40:22 +0500

    navigation done

--- src/navigations/appNavigator.js
+++ src/navigations/appNavigator.js
@@ -2,10 +2,9 @@ import { StyleSheet, Text, View } from "react-native";
 import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Login from "../screens/Login";
-import Signup from "../screens/Signup";
-import Home from "../screens/Home";
-import ForgotPassword from "../screens/Forgotpassword";
+import Index from "../screens/Index";
+import Playground from "../screens/Playground";
+
 
 const Stack = createNativeStackNavigator();
 
@@ -13,10 +12,8 @@ function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Signup" component={Signup} />
-        <Stack.Screen name="Home" component={Home} />
-        <Stack.Screen name="ForgotPassword" component={ForgotPassword} />
+        <Stack.Screen name="Index" component={Index} />
+        <Stack.Screen name="Playground" component={Playground} />
       </Stack.Navigator>
     </NavigationContainer>
   );
