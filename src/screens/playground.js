90244e8e8ed7bf381ccba050d0e83265505aac2b
shahbazqammar <shahbazqammar06@gmail.com>
2023-09-07 17:02:41 +0500

    without selecting submission update

--- src/screens/playground.js
+++ src/screens/playground.js
@@ -9,6 +9,7 @@ export default function Playground({ route }) {
   const [selectedOptions, setSelectedOptions] = useState({});
   const [score, setScore] = useState(0);
   const [showResults, setShowResults] = useState(false);
+  const [attemptedSubmit, setAttemptedSubmit] = useState(false); 
 
   const { category } = route.params  
 
@@ -39,15 +40,24 @@ export default function Playground({ route }) {
   };
 
   const handleOptionSelect = (questionIndex, option) => {
-    setSelectedOptions({
-      ...selectedOptions,
-      [questionIndex]: option,
-    }); 
+   
+      setSelectedOptions({
+        ...selectedOptions,
+        [questionIndex]: option,
+      });
+     
   };
+  
 
   const handleSubmit = () => {
+    const allQuestionsAnswered = questions.every((_, index) => selectedOptions[index] !== undefined);
+    if (!allQuestionsAnswered) {
+      // If not all questions are answered, show an alert and return
+      Alert.alert('Please select an option for all questions before submitting.');
+      return;
+    }
     let correctAnswers = 0;
-    questions.forEach((questions, index) => {
+    questions.forEach((question, index) => {
       if (selectedOptions[index] === question.correctOption) {
         correctAnswers++;
       }
@@ -55,6 +65,7 @@ export default function Playground({ route }) {
 
     setScore(correctAnswers);
     setShowResults(true);
+    setAttemptedSubmit(true); 
   };
 
   return (
@@ -64,29 +75,72 @@ export default function Playground({ route }) {
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item, index }) => (
           <View style={styles.questionContainer}>
-            <Text style={styles.question}>{item.Question}</Text>
+            <Text style={styles.question}>
+              {item.Question}
+              </Text>
 
-            {[1, 2, 3, 4].map(optionIndex => (
-              <TouchableOpacity
-                key={optionIndex}
-                style={[
-                  styles.option,
-                  selectedOptions[index] === optionIndex && styles.selectedOption,
-                  showResults && item.correctOption === optionIndex && styles.correctOption,
-                  showResults && selectedOptions[index] === optionIndex && selectedOptions[index] !== item.correctOption && styles.wrongOption,
-                ]}
-                onPress={() => handleOptionSelect(index, optionIndex)}
-                disabled={showResults}
-              >
-                <Text>{item['option' + optionIndex]}</Text>
-              </TouchableOpacity>
-            ))}
+            
+           <TouchableOpacity style={[
+            styles.option,
+         selectedOptions[index] === 1 && styles.selectedOptions,
+         showResults && item. correctOption === 1 && styles.correctOption,
+         showResults && selectedOptions[index] === 1 && selectedOptions[index] !== item.correctOption && styles.wrongOption,
+          ]}
+           onPress={() => handleOptionSelect(index, 1)}
+           disabled={showResults}
+         >
+           <Text>{item. option1}</Text>
+         </TouchableOpacity> 
+         <TouchableOpacity style={[
+            styles.option,
+         selectedOptions[index] === 2 && styles.selectedOptions,
+         showResults && item. correctOption === 2 && styles.correctOption,
+         showResults && selectedOptions[index] === 2 && selectedOptions[index] !== item.correctOption && styles.wrongOption,
+          ]}
+           onPress={() => handleOptionSelect(index, 2)}
+           disabled={showResults}
+         >
+           <Text>{item. option2}</Text>
+         </TouchableOpacity> 
+         <TouchableOpacity style={[
+            styles.option,
+         selectedOptions[index] === 3 && styles.selectedOptions,
+         showResults && item. correctOption === 3 && styles.correctOption,
+         showResults && selectedOptions[index] === 3 && selectedOptions[index] !== item.correctOption && styles.wrongOption,
+          ]}
+           onPress={() => handleOptionSelect(index, 3)}
+           disabled={showResults}
+         >
+           <Text>{item. option3}</Text>
+         </TouchableOpacity> 
+         <TouchableOpacity style={[
+            styles.option,
+         selectedOptions[index] === 4 && styles.selectedOptions,
+         showResults && item. correctOption === 4 && styles.correctOption,
+         showResults && selectedOptions[index] === 4 && selectedOptions[index] !== item.correctOption && styles.wrongOption,
+          ]}
+           onPress={() => handleOptionSelect(index, 4)}
+           disabled={showResults}
+         >
+           <Text>{item. option4}</Text>
+         </TouchableOpacity> 
           </View>
         )}
       />
       <TouchableOpacity style={styles.submitButton} onPress={handleSubmit} disabled={showResults}>
         <Text style={styles.submitButtonText}>Submit</Text>
       </TouchableOpacity>
+      {showResults && (
+  <View style={styles.result}>
+    <Text style={styles.resultText}>
+      Your score: {score} out of {questions.length}
+    </Text>
+    <TouchableOpacity style={styles.tryAgainButton} onPress={getQuestions}>
+      <Text style={styles.tryAgainButtonText}>Try Again</Text>
+    </TouchableOpacity>
+  </View>
+)}
+
     </View>
   );
 }
@@ -94,11 +148,28 @@ export default function Playground({ route }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor:'#f5f5f5',
     paddingHorizontal: 16,
     paddingTop: 24,
+    alignItems:'center',
+    justifyContent:'center',
   },
   questionContainer: {
     marginBottom: 20,
+    backgroundColor:'#f5f5f5',
+    borderRadius:10,
+    marginBottom:20,
+    padding:20,
+    shadowColor:'#000',
+    shadowOffset:{
+      width:0,
+      height:2,
+    },
+    shadowOpacity:0.25,
+    shadowRadius:3.85,
+    elevation:5,
+
+
   },
   question: {
     fontSize: 18,
@@ -106,13 +177,13 @@ const styles = StyleSheet.create({
     marginBottom: 10,
   },
   option: {
-    backgroundColor: 'lightgray',
+    backgroundColor: '#eee',
     padding: 10,
     marginVertical: 5,
     borderRadius: 5,
   },
-  selectedOption: {
-    backgroundColor: 'blue',
+  selectedOptions: {
+    backgroundColor: '#949494',
     // Adjust styles for selected option
   },
   correctOption: {
@@ -128,10 +199,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     padding: 10,
     borderRadius: 5,
-    marginTop: 20,
+    marginVertical: 20,
   },
   submitButtonText: {
     color: 'white',
     fontWeight: 'bold',
+    fontSize:20,
+  },
+  result:{
+     alignItems:'center',
+     justifyContent:'center',
+
+  },
+  resultText:{
+    fontSize:20,
+    fontWeight:'bold',
+    marginVertical:10,
+  },
+  tryAgainButton:{
+    backgroundColor:'blue',
+    padding:10,
+    marginVertical:20,
+    borderRadius:5,
+  },
+  tryAgainButtonText:{
+    color:'#fff',
+    fontSize:20,
+    fontWeight:'bold',
+    marginVertical:10,
   },
 });
